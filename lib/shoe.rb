8e9e9e90bb90c16ec39773709866a589f7fdc5a1
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-10-31 17:35:11 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,6 +2,47 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
+
+  def brand
+    @brand
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def color
+    @color
+  end
+
+  def size=(size)
+    @size = size
+  end
+
+  def size
+    @size
+  end
+
+  def material=(material)
+    @material = material
+  end
+
+  def material
+    @material
+  end
+
+  def condition=(condition)
+    @condition = condition
+  end
+
+  def condition
+    @condition
+  end
+
+  def cobble
+    puts "the shoe has been repaired"
+    @condition = new
+  end
 end
 
 
